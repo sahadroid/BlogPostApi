aa143e1940751cf2f2239e87a131c5a42d5cf126
sahadroid <sahat.2101@gmail.com>
2020-05-14 10:42:22 +0700

    first commit

---
+++ BlogPostApi/AppDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace BlogPostApi
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
